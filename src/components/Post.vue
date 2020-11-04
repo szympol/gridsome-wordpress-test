6ce7b2a07edaeaf9956eb78f4c51d60e308594f6
Szymon Polanowski <szympol@hotmail.com>
2020-11-04 15:12:38 +0100

    Fix media details

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -2,7 +2,7 @@
   <div>
     <h2 v-html="post.title" />
     <img
-      v-if="post.featuredMedia.mediaDetails"
+      v-if="post.featuredMedia && post.featuredMedia.mediaDetails"
       :src="post.featuredMedia.mediaDetails.sizes.medium.sourceUrl"
       :width="post.featuredMedia.mediaDetails.sizes.medium.width"
       :alt="post.featuredMedia.altText"
